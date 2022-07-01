1b23f176308c36da9ffb90a01f9322e88ea6b4c2
valerii <hannushchenko@gmail.com>
2022-07-01 03:38:20 +0300

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.port || 3000;
 
 app.get("/", (req, res) => {
   res.end("<h1>hello node world</h1>");
